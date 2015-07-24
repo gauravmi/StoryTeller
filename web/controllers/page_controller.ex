3510191d7458387cf443514456e7877e4a3d7677
Prasanth <prasantd@thoughtworks.com>
2015-07-24 22:50:19 +0530

    <Prasanth> Parsing sample JSON

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,6 +2,30 @@ defmodule StoryTeller.PageController do
   use StoryTeller.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+  	sampleJSON=Poison.Parser.parse!(~s({
+    "glossary": {
+        "title": "example glossary",
+		"GlossDiv": {
+            "title": "S",
+			"GlossList": {
+                "GlossEntry": {
+                    "ID": "SGML",
+					"SortAs": "SGML",
+					"GlossTerm": "Standard Generalized Markup Language",
+					"Acronym": "SGML",
+					"Abbrev": "ISO 8879:1986",
+					"GlossDef": {
+                        "para": "A meta-markup language, used to create markup languages such as DocBook.",
+						"GlossSeeAlso": ["GML", "XML"]
+                    },
+					"GlossSee": "markup"
+                }
+            }
+        }
+    }
+}))
+  	conn
+    |> put_status(201)
+    |> json sampleJSON
   end
 end
